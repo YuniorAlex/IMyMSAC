dd1c8b4538c4402bf89e9d7f2f1785e6b3ab9a3e
Yunior Alex <53721642+YuniorAlex@users.noreply.github.com>
2020-03-31 15:10:16 -0500

    Cambio en el diseño de la pagina

---
+++ IMM/Scripts/js/inicio.js
@@ -0,0 +1,14 @@
+﻿var menu = document.querySelector('nav.navbar');
+console.log(menu);
+
+function cambiarNav() {
+    if (document.documentElement.scrollTop > 0) {
+        menu.classList.add('deslizar');
+        console.log('hecho');
+    }
+    else {
+        menu.classList.remove('deslizar');
+    }
+}
+
+window.addEventListener('scroll', cambiarNav, false);
